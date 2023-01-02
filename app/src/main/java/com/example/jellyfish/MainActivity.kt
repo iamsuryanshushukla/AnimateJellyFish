58af0c312cc7f1197479e743750e26c8164be332
iamsuryanshushukla <suryanshushukla2001@gmail.com>
2023-01-02 15:54:46 +0530

    intial commit

---
+++ app/src/main/java/com/example/jellyfish/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jellyfish
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jellyfish.ui.theme.JellyFishTheme
+import com.example.jellyfish.ui.theme.JellyfishAnimation
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JellyFishTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    JellyfishAnimation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JellyFishTheme {
+        Greeting("Android")
+    }
+}
